b0a989c32627095d332f9c6c74a28892d5f5fea0
alksndrb <a.baljak.159@gmail.com>
2024-06-06 07:33:46 +0200

    next config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  distDir: "dist",
-  images: { unoptimized: true },
-};
+const nextConfig = {};
 
 export default nextConfig;
